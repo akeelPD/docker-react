b4eb66b4ebf586f4672651899179d269afc98bd3
Akeel <akeel.ameen@pleodata.com>
2021-06-28 08:06:35 +0800

    initial commit

---
+++ build/precache-manifest.216f80cb92c2559b2d21b9fa8f1204ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba2c33adf6e64ed32a3ef56ea2816fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74bf67c181c9d33d4569",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "e88cf915786643f1267f",
+    "url": "/static/js/2.56686953.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.56686953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74bf67c181c9d33d4569",
+    "url": "/static/js/main.7c3ded04.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
